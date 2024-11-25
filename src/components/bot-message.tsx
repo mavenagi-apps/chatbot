e42b201cd23ea8cd553c0d6e5673972b9b77b8b7
Xiao Shi <xiao@mavenagi.com>
2024-11-25 14:29:13 -0500

    add chart rendering
    Created using spr 1.3.5

--- src/components/bot-message.tsx
+++ src/components/bot-message.tsx
@@ -1,9 +1,33 @@
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import { BotChartResponse } from "mavenagi/api/resources/conversation/types/BotChartResponse";
+import { ChartSpecSchema } from "mavenagi/api/resources/conversation/types/ChartSpecSchema";
 import React from "react";
 
 import { ReactMarkdown } from "./react-markdown";
 
 export interface BotMessageProps {
   message: string;
+}
+
+export function RenderCharts({ charts }: { charts: BotChartResponse[] }) {
+  return charts.map((chart, index) => {
+    try {
+      // currently only handle HIGHCHARTS_TS charts
+      if (chart.specSchema !== ChartSpecSchema.HighchartsTs) {
+        return null;
+      }
+      const chartOptions = JSON.parse(chart.spec);
+      return (
+        <div key={index} id={`chart-${index}`}>
+          <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+        </div>
+      );
+    } catch (e) {
+      console.error(`Error rendering chart ${index}`, e);
+      return <div className="text-red-500">Error rendering chart</div>;
+    }
+  });
 }
 
 export function BotMessage({ message }: BotMessageProps) {
