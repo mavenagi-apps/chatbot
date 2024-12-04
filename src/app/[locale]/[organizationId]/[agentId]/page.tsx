a3429b3869c4ff8b0e4094b38bd5fe4526559aad
Amol Bhave <amol@amolbhave.com>
2024-12-04 08:15:12 -0600

    add better error page for not installed app

--- src/app/[locale]/[organizationId]/[agentId]/page.tsx
+++ src/app/[locale]/[organizationId]/[agentId]/page.tsx
@@ -1,9 +1,12 @@
 import Color from "colorjs.io";
-import { setRequestLocale } from "next-intl/server";
+import { MavenAGI } from "mavenagi";
+import { getTranslations, setRequestLocale } from "next-intl/server";
+import Image from "next/image";
 
 import { getClient } from "@/lib/client.server";
 
 import { Chat } from "./chat";
+import logoLight from "./mavenagi_logo_light.svg";
 
 export const revalidate = 60;
 export const dynamic = "force-static";
@@ -17,8 +20,51 @@ export default async function Page({
   const { locale, organizationId, agentId } = await params;
   setRequestLocale(locale);
   const client = getClient({ organizationId, agentId });
-  const appSettings =
-    (await client.appSettings.get()) as unknown as AppSettings;
+  const t = await getTranslations("chat.ChatPage");
+
+  const appSettings = await (async () => {
+    try {
+      return (await client.appSettings.get()) as unknown as AppSettings;
+    } catch (e) {
+      if (e instanceof MavenAGI.NotFoundError) {
+        return undefined;
+      }
+      throw e;
+    }
+  })();
+
+  if (!appSettings) {
+    return (
+      <main className="flex h-screen flex-col items-center justify-center gap-6 p-6">
+        <div className="flex flex-col gap-6 flex-1 items-center justify-center">
+          <h1 className="text-xl font-semibold tracking-tight text-balance text-gray-900 text-center">
+            Chat app is not installed.
+          </h1>
+          <p className="text-base font-medium text-pretty text-gray-500">
+            An error occurred in the application. If you are the owner of this
+            page, please make sure the Chat app is installed for your agent.
+          </p>
+        </div>
+        <div className="mx-auto flex items-center text-xs text-gray-400 justify-self-end self-end place-self-end">
+          {t("powered_by")}{" "}
+          <a
+            href="https://www.mavenagi.com"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Image
+              alt="Maven AGI"
+              src={logoLight}
+              className="ml-1 h-6"
+              width={82}
+              height={24}
+            />
+          </a>
+        </div>
+      </main>
+    );
+  }
+
   if (!appSettings.brandColor) {
     appSettings.brandColor = "#6C2BD9";
   }
