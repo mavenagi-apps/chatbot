0c17f35412bb713db8aeb512cfcd5f234d586813
Ben Yee <byee@mavenagi.com>
2025-01-23 19:28:11 -0800

    fix regression on active state (#8)

--- src/components/feedback-form.tsx
+++ src/components/feedback-form.tsx
@@ -101,8 +101,8 @@ export default function FeedbackForm({
               ref={thumbsUpRef}
               {...(feedbackTextFormShown &&
               feedbackType === FeedbackType.ThumbsUp
-                ? { "data-active": "" }
-                : {})}
+                ? { className: 'bg-gray-300' }
+                : { className: '' })}
               onClick={() => thumbsOnClick(FeedbackType.ThumbsUp)}
             >
               <ThumbsUp className="size-4" xlinkTitle={t("thumbs_up")} />
@@ -112,8 +112,8 @@ export default function FeedbackForm({
               ref={thumbsDownRef}
               {...(feedbackTextFormShown &&
               feedbackType === FeedbackType.ThumbsDown
-                ? { "data-active": "" }
-                : {})}
+                ? { className: 'bg-gray-300' }
+                : { className: '' })}
               onClick={() => thumbsOnClick(FeedbackType.ThumbsDown)}
             >
               <ThumbsDown className="size-4" xlinkTitle={t("thumbs_down")} />
